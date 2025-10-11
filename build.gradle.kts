603bbfc023ca1e8f8255a4d1868ece993508ccbe
Vitor Teofilo <vitor.teo1995@hotmail.com>
2025-10-11 20:59:06 -0300

    chore: initial commit with Spring Initializr project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.configfy"
+version = "0.0.1-SNAPSHOT"
+description = "Reactive backend API for configfy platform"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/com/configfy/configfyapi/ConfigfyapiApplication.java
@@ -0,0 +1,13 @@
+package com.configfy.configfyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigfyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigfyapiApplication.class, args);
+	}
+
+}
